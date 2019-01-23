1e8fa47d61d05e656cad87ec4ff5a91d31a14813
dmartin61@cnm.edu <danielleislesmartin@gmail.com>
2019-01-22 21:10:23 -0700

    fixed TODO's

--- bobs-burgers/bobs-burgers/Program.cs
+++ bobs-burgers/bobs-burgers/Program.cs
@@ -14,7 +14,7 @@ namespace bobs_burgers
 
             //process
             subtotal = 0.0;
-            finalTotal = createOrder(subtotal);
+            finalTotal = CreateOrder(subtotal);
 
             grandTotal = finalTotal + (finalTotal * tax);
 
@@ -25,51 +25,66 @@ namespace bobs_burgers
 
         }
 //TODO: Why are you passing in Subtotal here? Will it ever be non 0?
-        private static double createOrder(double subtotal)
+//Rebuttle: We set subtotal equal to 0.0 because when the program runs the user is starting a new order therefore their subtotal at start is 0 until the program conpiles the Menu and CreateOrder method will subtotal change base on user inputs. Also subtotal being 0 at the start of the program helped with the loop in CreateOrder working properly.
+        private static double CreateOrder(double subtotal)
         {
             //setup
-            string order ="";
             double burger = 2.99;
             double fries = 1.99;
             double drink = 0.99;
             double bobbyMeal = 5.00;
+            string answer = "";
+            Single order;
 
             //input
             do
             {
-//TODO: refactor menu into its own method single action priciple
-                //menu
+                order = Menu();
+
+                    if (order == 1)
+                    {
+                        subtotal = subtotal + burger;
+                    }
+                    else if (order == 2)
+                    {
+                        subtotal = subtotal + fries;
+                    }
+                    else if (order == 3)
+                    {
+                        subtotal = subtotal + drink;
+                    }
+                    else
+                    {
+                        subtotal = subtotal + bobbyMeal;
+                    }
+
+                //TODO: please validate your data input - done
+                do
+                {
+                    Console.WriteLine("Finished ordering? Yes = Y or No = N");
+                    answer = Console.ReadLine();
+                } while (answer.ToUpper() != "Y" && answer.ToUpper() != "N");
+            } while (answer.ToUpper() != "Y");
+
+            return (subtotal);
+        }
+
+        //TODO: refactor menu into its own method single action priciple -done
+        private static Single Menu()
+        {
+            Single orderMenu;
+            //TODO: please validate your user input - done
+            do
+            {
                 Console.WriteLine("Welcome to Bob's burgers!");
                 Console.WriteLine("1.) Burger = $2.99");
                 Console.WriteLine("2.) Fries = $1.99");
                 Console.WriteLine("3.) Drink = $.99");
                 Console.WriteLine("4.) Bobby Meal = $5.00 a burger, fry and drink");
-                
-//TODO: please validate your user input
-                order = Console.ReadLine();
 
-                if (order == "1")
-                {
-                    subtotal = subtotal + burger;
-                }
-                else if (order == "2")
-                {
-                    subtotal = subtotal + fries;
-                }
-                else if (order == "3")
-                {
-                    subtotal = subtotal + drink;
-                }
-                else
-                {
-                    subtotal = subtotal + bobbyMeal;
-                }
-//TODO: please validate your data input
-                Console.WriteLine("Finished ordering? Yes = Y, No = type a key");
-                order = Console.ReadLine();
-            } while (order.ToUpper() != "Y");
-
-            return (subtotal);
+                orderMenu = Single.Parse(Console.ReadLine());
+            } while (orderMenu != 1 && orderMenu != 2 && orderMenu != 3 && orderMenu != 4);
+            return orderMenu;
         }
     }
 }
